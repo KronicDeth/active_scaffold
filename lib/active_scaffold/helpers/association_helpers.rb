21352d1e00ba7f5c910b437e42a1013decd4b66b
Luke Imhoff <Kronic.Deth@gmail.com>
2010-12-08 14:44:16 -0600

    Add options_for_association_joins
    association_options_find and association_options_count only supported conditions, but a lot of conditions needed joins to make sense.  To work around not having joins, a separate full query with a join could be performed and then the id's from this query used as the condition, but it was inefficient, so add options_for_association_joins to go with options_for_association_conditions to add any necessary joins for the conditons to work.

--- lib/active_scaffold/helpers/association_helpers.rb
+++ lib/active_scaffold/helpers/association_helpers.rb
@@ -2,23 +2,50 @@ module ActiveScaffold
   module Helpers
     module AssociationHelpers
       # Provides a way to honor the :conditions on an association while searching the association's klass
-      def association_options_find(association, conditions = nil)
-        association.klass.find(:all, :conditions => controller.send(:merge_conditions, conditions, association.options[:conditions]))
+      def association_options_find(association, conditions = nil, joins = nil)
+        merged_conditions = controller.send(
+          :merge_conditions,
+          conditions,
+          association.options[:conditions]
+        )
+        association.klass.find(
+          :all,
+          :conditions => merged_conditions,
+          :joins => joins,
+          :select => "DISTINCT #{association.klass.table_name}.*"
+        )
       end
 
-      def association_options_count(association, conditions = nil)
-        association.klass.count(:all, :conditions => controller.send(:merge_conditions, conditions, association.options[:conditions]))
+      def association_options_count(association, conditions = nil, joins = nil)
+        merged_conditions = controller.send(
+          :merge_conditions,
+          conditions,
+          association.options[:conditions]
+        )
+        association.klass.count(
+          :all,
+          :conditions => merged_conditions,
+          :joins => joins
+        )
       end
 
       # returns options for the given association as a collection of [id, label] pairs intended for the +options_for_select+ helper.
       def options_for_association(association, include_all = false)
-        available_records = association_options_find(association, include_all ? nil : options_for_association_conditions(association))
+        available_records = association_options_find(
+          association,
+          include_all ? nil : options_for_association_conditions(association),
+          options_for_association_joins(association)
+        )
         available_records ||= []
         available_records.sort{|a,b| a.to_label <=> b.to_label}.collect { |model| [ model.to_label, model.id ] }
       end
 
       def options_for_association_count(association)
-        association_options_count(association, options_for_association_conditions(association))
+        association_options_count(
+          association,
+          options_for_association_conditions(association),
+          options_for_association_joins(association)
+        )
       end
 
       # A useful override for customizing the records present in an association dropdown.
@@ -34,6 +61,10 @@ module ActiveScaffold
             # Find all
             nil
         end
+      end
+      
+      def options_for_association_joins(association)
+        nil
       end
     end
   end
